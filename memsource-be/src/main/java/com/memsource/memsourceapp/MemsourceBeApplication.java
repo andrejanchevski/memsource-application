2e2ba5bff1850779c4b875b9a088d7c6314fec8b
andrejancevski <andrej.ancevski9@gmail.com>
2022-05-17 13:40:27 +0200

    [MEMSOURCE_TASK] - Added inital project structure for Backend Project

---
+++ memsource-be/src/main/java/com/memsource/memsourceapp/MemsourceBeApplication.java
@@ -0,0 +1,13 @@
+package com.memsource.memsourceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemsourceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemsourceBeApplication.class, args);
+    }
+
+}
---
+++ memsource-be/src/test/java/com/memsource/memsourceapp/MemsourceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memsource.memsourceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemsourceBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
