c6bd6617679bcd381c34576355e1813d1b9ec461
andrejancevski <andrej.ancevski9@gmail.com>
2022-05-21 23:05:11 +0200

    [MEMSOURCE_TASK] - Fixed event listener not working for persisting the token to file

--- memsource-be/src/main/java/com/memsource/memsourceapp/listeners/PersistAuthenticationTokenEventListener.java
+++ memsource-be/src/main/java/com/memsource/memsourceapp/listeners/PersistAuthenticationTokenEventListener.java
@@ -3,17 +3,21 @@ package com.memsource.memsourceapp.listeners;
 import com.memsource.memsourceapp.events.PersistAuthenticationTokenEvent;
 import com.memsource.memsourceapp.exceptions.PersistingTokenToFileException;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+@Component
 public class PersistAuthenticationTokenEventListener implements ApplicationListener<PersistAuthenticationTokenEvent> {
     @Override
     public void onApplicationEvent(PersistAuthenticationTokenEvent event) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("memsource_application_token"));
             writer.write(event.getApiClientAuthenticationToken());
+            writer.flush();
+            writer.close();
         } catch (IOException e) {
             throw new PersistingTokenToFileException(e.getMessage());
         }
